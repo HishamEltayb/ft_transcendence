0147b2001987b4c5cda6efb1128af29dacfff8bc
Hisham042 <zqhisham@hotmail.com>
2025-04-07 15:48:29 +0400

    added sql structure

--- Backend/users/models.py
+++ Backend/users/models.py
@@ -8,3 +8,24 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+
+#postgres sql table structure
+
+# CREATE TABLE users_user (
+#     id SERIAL PRIMARY KEY,
+#     username VARCHAR(150) UNIQUE NOT NULL,
+#     password VARCHAR(128) NOT NULL,
+#     first_name VARCHAR(150),
+#     last_name VARCHAR(150),
+#     is_active BOOLEAN NOT NULL,
+#     is_staff BOOLEAN NOT NULL,
+#     is_superuser BOOLEAN NOT NULL,
+#     date_joined TIMESTAMP NOT NULL,
+#     last_login TIMESTAMP,
+#     -- Your custom fields:
+#     email VARCHAR(254) UNIQUE NOT NULL,
+#     bio TEXT,
+#     profile_image VARCHAR(255)
+# );
