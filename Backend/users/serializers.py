65b7551a0df6e3ae5927cc4e700cf97e6fcd3908
dice42 <mohammoh@student.42abudhabi.ae>
2025-04-21 17:31:27 +0400

    serializers to check the newpassword with the old password

--- Backend/users/serializers.py
+++ Backend/users/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import User, PlayerProfile
 from django.contrib.auth.password_validation import validate_password
+from django.core.validators import EmailValidator
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -35,3 +36,28 @@ class PlayerProfileSerializer(serializers.ModelSerializer):
         model = PlayerProfile
         fields = ['id', 'username', 'total_games', 'wins', 'losses', 'rank', 'win_rate', 'avatar']
         read_only_fields = ['id', 'win_rate']
+
+
+class PasswordResetRequestSerializer(serializers.Serializer):
+    email = serializers.EmailField(validators=[EmailValidator()])
+
+
+class PasswordResetConfirmSerializer(serializers.Serializer):
+    token = serializers.CharField()
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
+    
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({"password": "Password fields didn't match."})
+        return attrs
+
+
+class ChangePasswordSerializer(serializers.Serializer):
+    new_password = serializers.CharField(required=True, validators=[validate_password])
+    new_password2 = serializers.CharField(required=True)
+    
+    def validate(self, attrs):
+        if attrs['new_password'] != attrs['new_password2']:
+            raise serializers.ValidationError({"new_password": "Password fields didn't match."})
+        return attrs
