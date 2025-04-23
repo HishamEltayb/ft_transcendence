2c68c09fc43c3c864240b6b80a2ef69d58e8f920
dice42 <mohammoh@student.42abudhabi.ae>
2025-04-23 20:28:45 +0400

    updating the serializers

--- Backend/users/serializers.py
+++ Backend/users/serializers.py
@@ -1,12 +1,17 @@
 from rest_framework import serializers
-from .models import User, PlayerProfile
 from django.contrib.auth.password_validation import validate_password
+from .models import User
+from urllib.parse import urlparse
+from django.conf import settings
 
 class UserSerializer(serializers.ModelSerializer):
+    win_rate = serializers.ReadOnlyField()
+    profile_image = serializers.SerializerMethodField()
+
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'bio', 'profile_image', 'intra_id', 'intra_login', 'is_oauth_user', 'is_two_factor_enabled']
-        read_only_fields = ['id', 'intra_id', 'intra_login', 'is_oauth_user']
+        fields = ['id', 'username', 'email', 'bio', 'profile_image', 'intra_id', 'intra_login', 'is_oauth_user', 'is_two_factor_enabled', 'total_games', 'wins', 'losses', 'rank', 'win_rate']
+        read_only_fields = ['id', 'intra_id', 'intra_login', 'is_oauth_user', 'win_rate']
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
@@ -29,9 +34,22 @@ class RegisterSerializer(serializers.ModelSerializer):
 class PlayerProfileSerializer(serializers.ModelSerializer):
     username = serializers.ReadOnlyField(source='user.username')
     win_rate = serializers.ReadOnlyField()
-    avatar = serializers.ImageField()
+    profile_image = serializers.SerializerMethodField()
     
     class Meta:
-        model = PlayerProfile
-        fields = ['id', 'username', 'total_games', 'wins', 'losses', 'rank', 'win_rate', 'avatar']
+        model = User
+        fields = ['id', 'username', 'total_games', 'wins', 'losses', 'rank', 'win_rate', 'profile_image']
         read_only_fields = ['id', 'win_rate']
+
+    def get_profile_image(self, obj):
+        if obj.profile_image:
+            if hasattr(obj.profile_image, 'url'):
+                request = self.context.get('request')
+                if request:
+                    return request.build_absolute_uri(obj.profile_image.url)
+                return obj.profile_image.url
+            elif isinstance(obj.profile_image, str):
+                parsed_url = urlparse(obj.profile_image)
+                if parsed_url.scheme and parsed_url.netloc:
+                    return obj.profile_image
+        return None
