ffa421c256a83710dac05aa31e43290b12e7f0b6
hishameltayb <zqhisham@gmail.com>
2025-04-26 11:44:24 +0400

    updated the admin site for the user

--- Backend/users/admin.py
+++ Backend/users/admin.py
@@ -4,10 +4,11 @@ from .models import User
 
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
-    list_display = ('username', 'email', 'id')
+    list_display = ('username', 'email', 'id', 'is_two_factor_enabled', 'is_oauth_user')
     ordering = ('date_joined',)
     
     fieldsets = (
         ('User Credentials', {'fields': ('username', 'password')}),
-        ('Personal info', {'fields': ('first_name', 'last_name', 'email', 'profile_image')}),
+        ('Personal info', {'fields': ('email', 'profile_image', 'is_two_factor_enabled', 'is_oauth_user', 'state')}),
+        ('game info', {'fields': ('wins', 'losses', 'total_games', 'rank', 'win_rate')})
     )
