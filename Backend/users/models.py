20af4c567e098f0923b51c5ee2d0df6164565928
hishameltayb <zqhisham@gmail.com>
2025-04-26 13:59:04 +0400

    cleanup

--- Backend/users/models.py
+++ Backend/users/models.py
@@ -32,21 +32,10 @@ class User(AbstractUser):
 #     id SERIAL PRIMARY KEY,
 #     username VARCHAR(150) UNIQUE NOT NULL,
 #     password VARCHAR(128) NOT NULL,
-#     first_name VARCHAR(150),
-#     last_name VARCHAR(150),
-#     is_active BOOLEAN NOT NULL,
-#     is_staff BOOLEAN NOT NULL,
-#     is_superuser BOOLEAN NOT NULL,
-#     date_joined TIMESTAMP NOT NULL,
-#     last_login TIMESTAMP,
-#     -- Your custom fields:
 #     email VARCHAR(254) UNIQUE NOT NULL,
-#     bio TEXT,
 #     profile_image VARCHAR(255),
 #     -- 42 OAuth fields:
 #     intra_id VARCHAR(100) UNIQUE,
 #     intra_login VARCHAR(100),
 #     is_oauth_user BOOLEAN NOT NULL DEFAULT FALSE
-# );
-# );
 # );
