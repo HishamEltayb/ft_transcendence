da445bb1a94d4ad33cb1d3212937715c185e8409
hishameltayb <zqhisham@gmail.com>
2025-04-26 12:22:31 +0400

    remove the win_rate and the state from the user model

--- Backend/users/models.py
+++ Backend/users/models.py
@@ -9,19 +9,15 @@ class User(AbstractUser):
     intra_id = models.CharField(max_length=100, blank=True, null=True, unique=True)
     intra_login = models.CharField(max_length=100, blank=True, null=True)
     is_oauth_user = models.BooleanField(default=False)
-    state = models.CharField(max_length=10, choices=[('OF', 'Offline'), ('ON', 'Online'), ('ID', 'In-Game')], default='OF')
     
     # 2FA fields
     is_two_factor_enabled = models.BooleanField(default=False)
+
+    # Game related fields
     total_games = models.IntegerField(default=0)
     wins = models.IntegerField(default=0)
     losses = models.IntegerField(default=0)
     rank = models.IntegerField(default=1000)  # ELO rating starting at 1000
-    
-    @property
-    def win_rate(self):
-        if self.total_games > 0:
-            return round((self.wins / self.total_games) * 100, 2)
 
     def __str__(self):
         return self.username
