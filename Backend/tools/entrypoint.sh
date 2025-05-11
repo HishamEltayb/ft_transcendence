f87a77724e3760a8e8d279ec2f8ce369644b7052
hishameltayb <zqhisham@gmail.com>
2025-05-11 13:37:37 +0400

    Simplify blockchain setup and remove ELK stack integration

--- Backend/tools/entrypoint.sh
+++ Backend/tools/entrypoint.sh
@@ -1,18 +1,14 @@
 #!/bin/sh
 
-set -e # Exit immediately if a command exits with a non-zero status.
-
 python manage.py makemigrations
 python manage.py migrate
-
-python manage.py index_users
 
 python manage.py collectstatic --noinput
 
 # Create superuser if DJANGO_SUPERUSER variables are set
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] && [ -n "$DJANGO_SUPERUSER_EMAIL" ]; then
-    echo "Creating superuser..."
-    python manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL
+        echo "Creating superuser..."
+        python manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL
 fi
 
 # Deploy blockchain contract
@@ -21,9 +17,4 @@ python /app/tournaments/deploy_chain.py
 
 # Start server
 echo "Starting server..."
-python manage.py runserver 0.0.0.0:8000&
-
-# Wait a bit for Kibana to be up, then create the index pattern
-python tools/create_kibana_index_pattern.py
-
-wait
+python manage.py runserver 0.0.0.0:8000
